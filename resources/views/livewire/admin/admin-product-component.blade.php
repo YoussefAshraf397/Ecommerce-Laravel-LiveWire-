14cda5e3cda416348ddc4b7caeb054e85ffd8f2b
YoussefAshraf397 <youssefashraf397@yahoo.com>
2022-01-25 19:54:51 +0200

    valdite are you want delete or not

--- resources/views/livewire/admin/admin-product-component.blade.php
+++ resources/views/livewire/admin/admin-product-component.blade.php
@@ -52,7 +52,7 @@
                                     <td>{{$product->created_at}}</td>
                                     <td>
                                         <a href="{{route('admin.editproduct' , ['product_slug' => $product->slug])}}" ><i class="fa fa-edit fa-2x"></i></a>
-                                        <a href="#" wire:click.prevent="deleteProduct('{{$product->id}}')" style="margin-left: 10px" ><i class="fa fa-times fa-2x text-danger"></i></a>
+                                        <a href="#"  onclick="confirm('Are You Sure, you want delete this Product?') || event.stopImmediatePropagation()"  wire:click.prevent="deleteProduct('{{$product->id}}')" style="margin-left: 10px" ><i class="fa fa-times fa-2x text-danger"></i></a>
                                     </td>
                                 </tr>
                             @endforeach
